14042db1b4d565681298af423a022554610e2ec0
Diego VillatoroMx <107954399+diegovillatoromx@users.noreply.github.com>
2024-02-06 12:41:00 -0600

    Create copy_data_to_tables.sql

---
+++ Airflow-ETL-Pipeline-for-Snowflake/snowflake_insfrastructure/copy_data_to_tables.sql
@@ -0,0 +1,27 @@
+-- Copy data from Parquet files in S3 to 'users' table
+COPY INTO your_schema_name.users
+  FROM @your_snowflake_stage
+  FILE_FORMAT = (TYPE = 'PARQUET')
+  PATTERN = 'users/*.parquet'
+  ON_ERROR = 'CONTINUE';
+
+-- Copy data from Parquet files in S3 to 'events' table
+COPY INTO your_schema_name.events
+  FROM @your_snowflake_stage
+  FILE_FORMAT = (TYPE = 'PARQUET')
+  PATTERN = 'events/*.parquet'
+  ON_ERROR = 'CONTINUE';
+
+-- Copy data from Parquet files in S3 to 'orders' table
+COPY INTO your_schema_name.orders
+  FROM @your_snowflake_stage
+  FILE_FORMAT = (TYPE = 'PARQUET')
+  PATTERN = 'orders/*.parquet'
+  ON_ERROR = 'CONTINUE';
+
+-- Copy data from Parquet files in S3 to 'purchase_counts' table
+COPY INTO your_schema_name.purchase_counts
+  FROM @your_snowflake_stage
+  FILE_FORMAT = (TYPE = 'PARQUET')
+  PATTERN = 'purchase_counts/*.parquet'
+  ON_ERROR = 'CONTINUE';
