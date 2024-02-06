c79943d705176664737838c2107a2f2a73d48d79
Diego VillatoroMx <107954399+diegovillatoromx@users.noreply.github.com>
2024-02-06 12:21:06 -0600

    Create s3_to_snowflake.sql

---
+++ Airflow-ETL-Pipeline-for-Snowflake/snowflake_insfrastructure/s3_to_snowflake.sql
@@ -0,0 +1,8 @@
+-- Create a stage using the IAM role
+CREATE OR REPLACE STAGE your_stage_name
+URL = 's3://your_s3_bucket/'
+CREDENTIALS = (
+  AWS_ROLE = 'arn:aws:iam::your_aws_account_id:role/SnowflakeAccessRole'
+);
+
+GRANT READ ON STAGE your_stage_name TO ROLE SnowflakeAccessRole;
