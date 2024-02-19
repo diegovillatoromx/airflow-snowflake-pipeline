f559005f3aef1525e168f74e82e4704a0af4d57d
Diego VillatoroMx <107954399+diegovillatoromx@users.noreply.github.com>
2024-02-19 11:20:52 -0600

    Update new_role.py

--- Airflow-ETL-Pipeline-for-Snowflake/aws_infrastructure/new_role.py
+++ Airflow-ETL-Pipeline-for-Snowflake/aws_infrastructure/new_role.py
@@ -4,7 +4,7 @@ import json
 # AWS credentials
 aws_access_key_id = 'YOUR_AWS_ACCESS_KEY'
 aws_secret_access_key = 'YOUR_AWS_SECRET_KEY'
-
+ 
 # Create a new AWS IAM client
 iam_client = boto3.client(
     'iam',
