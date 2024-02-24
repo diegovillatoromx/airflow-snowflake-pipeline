35cb859267aeffed61830c1bdf381e351811c254
Diego VillatoroMx <107954399+diegovillatoromx@users.noreply.github.com>
2024-02-24 14:00:46 -0600

    Update s3_to_snowflake.sql

--- Airflow-ETL-Pipeline-for-Snowflake/snowflake_insfrastructure/s3_to_snowflake.sql
+++ Airflow-ETL-Pipeline-for-Snowflake/snowflake_insfrastructure/s3_to_snowflake.sql
@@ -3,6 +3,6 @@ CREATE OR REPLACE STAGE your_stage_name
 URL = 's3://your_s3_bucket/'
 CREDENTIALS = (
   AWS_ROLE = 'arn:aws:iam::your_aws_account_id:role/SnowflakeAccessRole'
-);
+); 
 
 GRANT READ ON STAGE your_stage_name TO ROLE SnowflakeAccessRole;
