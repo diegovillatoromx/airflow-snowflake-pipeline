077586f136880fc0fb5d28c533c5e83a633f0811
Diego VillatoroMx <107954399+diegovillatoromx@users.noreply.github.com>
2024-02-24 13:57:59 -0600

    Update s3_DAG.py

--- Airflow-ETL-Pipeline-for-Snowflake/aws_infrastructure/s3_DAG.py
+++ Airflow-ETL-Pipeline-for-Snowflake/aws_infrastructure/s3_DAG.py
@@ -1,6 +1,6 @@
 import boto3
 
-# Set your AWS credentials (you can also use environment variables or IAM roles)
+# Set your AWS credentials (you can also use environment variables or IAM roles)  
 aws_access_key_id = 'YOUR_ACCESS_KEY_ID' 
 aws_secret_access_key = 'YOUR_SECRET_ACCESS_KEY'
 
