216c8f630259caa56e87df3514ec22dc62c50550
Diego VillatoroMx <107954399+diegovillatoromx@users.noreply.github.com>
2024-02-27 15:24:43 -0600

    Update airflow_enviroment.py

--- Airflow-ETL-Pipeline-for-Snowflake/aws_infrastructure/airflow_enviroment.py
+++ Airflow-ETL-Pipeline-for-Snowflake/aws_infrastructure/airflow_enviroment.py
@@ -4,7 +4,7 @@ import boto3
 client = boto3.client('mwaa', region_name='us-east-1')  # Replace 'us-east-1' with your preferred region
 
 # Define Apache Airflow environment configuration 
-environment = {
+environment = { 
     'Name': 'my-airflow-environment',  # Change this to your desired name
     'ExecutionRoleArn': 'arn:aws:iam::123456789012:role/service-role/my-mwaa-role',  # Change this to your execution role ARN
     'DagS3Path': 's3://my-dag-bucket',  # Change this to the location of your S3 DAG bucket
